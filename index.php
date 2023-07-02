ca6f1170d0db8ab4ed75b4e9321b9b2a8caa3da6
Alexandr Dikarev <enotov21@yandex.ru>
2023-07-02 03:36:39 +0300

    qwe

--- index.php
+++ index.php
@@ -7,20 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-  <ul class="nav">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Active</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Организация</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">компания</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled">Disabled</a>
-  </li>
-</ul>
+  <?php require_once 'menu/menu.php' ?>
   <div class="container">
   <div class="card">
     <div class="card-header">123</div>
---
+++ menu/menu.php
@@ -0,0 +1,14 @@
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="../index.php">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../site/site.php">Организация</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../site/site2.php">Компания</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
